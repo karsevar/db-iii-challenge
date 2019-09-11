cd0758bc073cae60b9663aeee60159e23f82123d
masonkarsevar <masonkarsevar@gmail.com>
2019-09-11 12:14:25 -0700

    findSteps function is now working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,4 +8,11 @@ function findById(id) {
     return db('schemes').where({id})
 }
 
-module.exports = {find, findById}
+function findSteps(id) {
+    return db('steps as st')
+            .join('schemes as sc', 'st.scheme_id', '=', 'sc.id')
+            .where({scheme_id: id})
+            .select('st.id','sc.scheme_name', 'st.step_number', 'st.instructions')
+}
+
+module.exports = {find, findById, findSteps}
